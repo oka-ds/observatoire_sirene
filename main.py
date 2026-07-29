d9a333234f1aa90e426bd12e70eb30d7fb7b2a6e
Millord1 <roffat.theau@outlook.com>
2026-07-29 12:27:05 +0200

    fix main

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from historisation import push_scd2
 def main():
     db = DatabaseManager()
     db.refresh_observatoire()
-    # raw_rows = db.count_rows(schema_name=config.SCHEMA_NAME, table_name=config.TABLE_NAME)
-    # if raw_rows == 0:
-    #     load_etab('69')
+    raw_rows = db.count_rows(schema_name=config.SCHEMA_PUBLIC_NAME, table_name=config.TABLE_NAME)
+    if raw_rows == 0:
+        load_etab('69')
         
-    # warehouse_rows = db.count_rows(schema_name=config.WAREHOUSE_SCHEMA, table_name='fait_etablissement_version')
-    # if warehouse_rows == 0:
-    #     push_scd2(config.SCHEMA_PUBLIC_NAME, config.TABLE_NAME)
+    warehouse_rows = db.count_rows(schema_name=config.WAREHOUSE_SCHEMA, table_name='fait_etablissement_version')
+    if warehouse_rows == 0:
+        push_scd2(config.SCHEMA_PUBLIC_NAME, config.TABLE_NAME)
         
     push_scd2(config.SCHEMA_PUBLIC_NAME, config.TABLE_NAME)
     
