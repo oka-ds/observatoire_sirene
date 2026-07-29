bb11e8df5f913390fc9891e98365e4491faecb86
Millord1 <roffat.theau@outlook.com>
2026-07-29 11:13:35 +0200

    ingestion

--- ingestion.py
+++ ingestion.py
@@ -6,37 +6,36 @@ import config
 
 load_dotenv()
 
-def load_etab(histo: bool, code_dept: str = None):
+def load_etab(code_dept: str = None):
     
-    PARQUET_URL = config.HISTORIQUE_PARQUET_URL if histo else config.PARQUET_URL
-    TABLE_NAME = config.TABLE_HISTO_NAME if histo else config.TABLE_NAME
+    PARQUET_URL = config.PARQUET_URL
+    PARQUET_HISTO_URL = config.HISTORIQUE_PARQUET_URL
+    
+    TABLE_NAME = config.TABLE_NAME
     SCHEMA_NAME = config.SCHEMA_PUBLIC_NAME
 
     connexion_duckdb = duckdb.connect()
     db_url = os.getenv("DATABASE_URL")
-    
-    where_sql = ""
-    
-    if histo:
-        where_sql = ""
-    else:
-        where_clauses = ["statutDiffusionEtablissement = 'O'"]
-        if code_dept:
-            where_clauses.append(f"LEFT(codeCommuneEtablissement, 2) = '{code_dept}'")
-        where_sql = "WHERE " + " AND ".join(where_clauses)
 
-    query = f"""
-    INSTALL httpfs;
-    LOAD httpfs;
-    INSTALL postgres;
-    LOAD postgres;
-    ATTACH '{db_url}' AS pg_db (TYPE POSTGRES);
+    connexion_duckdb.execute("INSTALL httpfs")
+    connexion_duckdb.execute("LOAD httpfs")
+    connexion_duckdb.execute("INSTALL postgres")
+    connexion_duckdb.execute("LOAD postgres")
+    connexion_duckdb.execute(f"ATTACH '{db_url}' AS pg_db (TYPE POSTGRES);")
     
+    where_clauses = ["a.statutDiffusionEtablissement = 'O'"]
+    if code_dept:
+        where_clauses.append(f"LEFT(a.codeCommuneEtablissement, 2) = '{code_dept}'")
+    where_sql = "WHERE " + " AND ".join(where_clauses)
+    
+    query = f"""
     DROP TABLE IF EXISTS pg_db.{SCHEMA_NAME}.{TABLE_NAME};
     
     CREATE TABLE pg_db.{SCHEMA_NAME}.{TABLE_NAME} AS 
-    SELECT *
-    FROM read_parquet('{PARQUET_URL}')
+    SELECT h.*
+    FROM read_parquet('{PARQUET_HISTO_URL}') h
+    INNER JOIN read_parquet('{PARQUET_URL}') a 
+        ON h.siret = a.siret
     {where_sql};
     """
     
@@ -46,6 +45,5 @@ def load_etab(histo: bool, code_dept: str = None):
     print('done')
     
 if __name__ == "__main__":
-    arg = sys.argv[1].lower() == 'true' if len(sys.argv) > 1 else False
-    dept = sys.argv[2] if len(sys.argv) > 2 else None
-    load_etab(arg, dept)
+    dept = sys.argv[1] if len(sys.argv) > 1 else None
+    load_etab(dept)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from load import DatabaseManager
 
 def main():
     DatabaseManager()
-    load_etab(histo=True, code_dept='69')
+    load_etab('69')
     
 if __name__ == "__main__":
     main()
