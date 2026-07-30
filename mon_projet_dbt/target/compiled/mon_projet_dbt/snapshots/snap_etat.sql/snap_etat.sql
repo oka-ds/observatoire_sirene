e60d79ecbfab4e37e43b2c1a1cdfaceee9d14cae
jacQdo <jacqdo@gmail.com>
2026-07-30 17:03:18 +0200

    Step2 Pass1 - Mise en place dbt (sources, staging, tests, snapshots)

---
+++ mon_projet_dbt/target/compiled/mon_projet_dbt/snapshots/snap_etat.sql/snap_etat.sql
@@ -0,0 +1,3 @@
+
+
+select siret, etat, code_ape from "observatoire_sirene"."observatoire"."etats_demo"
---
+++ mon_projet_dbt/target/run/mon_projet_dbt/snapshots/snap_etat.sql/snap_etat.sql
@@ -0,0 +1,17 @@
+
+      update "observatoire_sirene"."observatoire"."snap_etat"
+    set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
+    from "snap_etat__dbt_tmp165636781207" as DBT_INTERNAL_SOURCE
+    where DBT_INTERNAL_SOURCE.dbt_scd_id::text = "observatoire_sirene"."observatoire"."snap_etat".dbt_scd_id::text
+      and DBT_INTERNAL_SOURCE.dbt_change_type::text in ('update'::text, 'delete'::text)
+      
+        and "observatoire_sirene"."observatoire"."snap_etat".dbt_valid_to is null;
+      
+
+
+    insert into "observatoire_sirene"."observatoire"."snap_etat" ("siret", "etat", "code_ape", "dbt_updated_at", "dbt_valid_from", "dbt_valid_to", "dbt_scd_id")
+    select DBT_INTERNAL_SOURCE."siret",DBT_INTERNAL_SOURCE."etat",DBT_INTERNAL_SOURCE."code_ape",DBT_INTERNAL_SOURCE."dbt_updated_at",DBT_INTERNAL_SOURCE."dbt_valid_from",DBT_INTERNAL_SOURCE."dbt_valid_to",DBT_INTERNAL_SOURCE."dbt_scd_id"
+    from "snap_etat__dbt_tmp165636781207" as DBT_INTERNAL_SOURCE
+    where DBT_INTERNAL_SOURCE.dbt_change_type::text = 'insert'::text;
+
+  
