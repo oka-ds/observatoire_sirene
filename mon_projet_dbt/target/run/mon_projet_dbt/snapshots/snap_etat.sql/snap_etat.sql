f52d8248740b9fc1b03458e617e5eb7890423b16
jacQdo <jacqdo@gmail.com>
2026-07-31 11:50:22 +0200

    doc rapport

--- mon_projet_dbt/target/run/mon_projet_dbt/snapshots/snap_etat.sql/snap_etat.sql
+++ mon_projet_dbt/target/run/mon_projet_dbt/snapshots/snap_etat.sql/snap_etat.sql
@@ -1,7 +1,7 @@
 
       update "observatoire_sirene"."observatoire"."snap_etat"
     set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
-    from "snap_etat__dbt_tmp165636781207" as DBT_INTERNAL_SOURCE
+    from "snap_etat__dbt_tmp170656615473" as DBT_INTERNAL_SOURCE
     where DBT_INTERNAL_SOURCE.dbt_scd_id::text = "observatoire_sirene"."observatoire"."snap_etat".dbt_scd_id::text
       and DBT_INTERNAL_SOURCE.dbt_change_type::text in ('update'::text, 'delete'::text)
       
@@ -11,7 +11,7 @@
 
     insert into "observatoire_sirene"."observatoire"."snap_etat" ("siret", "etat", "code_ape", "dbt_updated_at", "dbt_valid_from", "dbt_valid_to", "dbt_scd_id")
     select DBT_INTERNAL_SOURCE."siret",DBT_INTERNAL_SOURCE."etat",DBT_INTERNAL_SOURCE."code_ape",DBT_INTERNAL_SOURCE."dbt_updated_at",DBT_INTERNAL_SOURCE."dbt_valid_from",DBT_INTERNAL_SOURCE."dbt_valid_to",DBT_INTERNAL_SOURCE."dbt_scd_id"
-    from "snap_etat__dbt_tmp165636781207" as DBT_INTERNAL_SOURCE
+    from "snap_etat__dbt_tmp170656615473" as DBT_INTERNAL_SOURCE
     where DBT_INTERNAL_SOURCE.dbt_change_type::text = 'insert'::text;
 
   
